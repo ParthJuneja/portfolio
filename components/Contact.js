e7c60906d391088b864ff9314e607bdafcaa14e3
parth <admin@test.com>
2023-03-04 20:12:50 +0530

    class changed

--- components/Contact.js
+++ components/Contact.js
@@ -3,25 +3,25 @@ import React from "react";
 function Contact() {
   return (
     <section className="">
-      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-light_front dark:text-fore">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-light_front dark:text-fore">
           Contact Me !
         </h2>
-        <p class="mb-8 lg:mb-16 text-center text-light_mid2 dark:text-pale sm:text-xl">
+        <p className="mb-8 lg:mb-16 text-center text-light_mid2 dark:text-pale sm:text-xl">
           Have a question ? Get in touch with me{" "}
         </p>
-        <form action="#" class="space-y-8">
+        <form action="#" className="space-y-8">
           <div>
             <label
               for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Your email
             </label>
             <input
               type="email"
               id="email"
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@email.com"
               required
             />
@@ -29,19 +29,19 @@ function Contact() {
           <div>
             <label
               for="subject"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Subject
             </label>
             <input
               type="text"
               id="subject"
-              class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Let me know how i can help"
               required
             />
           </div>
-          <div class="sm:col-span-2">
+          <div className="sm:col-span-2">
             <label
               for="message"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
@@ -51,7 +51,7 @@ function Contact() {
             <textarea
               id="message"
               rows="6"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-mid1 dark:border-earth dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Your feedback is highly appreciated.."
             ></textarea>
           </div>
